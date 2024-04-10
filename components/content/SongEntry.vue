3fe3bd3dc3511fb26d40ebfc366e9e1dfc71767c
Minh Hikari <minhhikari1994+dev@outlook.com>
2024-04-10 21:22:17 +0700

    fix song, movie and tvseries width

--- components/content/SongEntry.vue
+++ components/content/SongEntry.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="box m-auto w-9 my-3">
+    <div class="box m-auto xl:w-9 w-12 my-3">
         <div class="grid">
             <div class="col-12 lg:col-4 ">
                 <img class="w-full" :src="image" :alt="name" />
