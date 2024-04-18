59ae0ac85192a6e6682a5b16934d0a81d93d1e51
Minh Hikari <97784261+minhhikari1994@users.noreply.github.com>
2024-04-18 23:53:58 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import {resolve} from 'path'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/ui', '@nuxtjs/i18n', '@nuxthq/studio', 'nuxt-cron', '@nuxtjs/seo'],
+  modules: ['@nuxt/content', '@nuxt/ui', '@nuxtjs/i18n', '@nuxthq/studio', 'nuxt-cron'],
   css: [
     '~/assets/css/main.css',
     'primeflex/primeflex.css',
