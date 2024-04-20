7c0e3adeb630566d59aedfbe5f94837e5ce10ae3
Minh Hikari <minhhikari1994+dev@outlook.com>
2024-04-20 21:41:46 +0700

    Complete cronjob implementation

--- server/cron/animeWatchScheduleCron.js
+++ server/cron/animeWatchScheduleCron.js
@@ -1,10 +1,8 @@
 import fs from 'fs'
-import { defineCronHandler } from '#nuxt/cron';
+import { defineCronHandler } from '#nuxt/cron'
 import dailyAnimeIDs from './dailyAnimeIDs.json'
-import MALData from './MALResponseData.json';
 
-const populateAnimeListForWeekDay = (weekDayKey, animeIDs) => {
-    
+const populateAnimeListForWeekDay = (weekDayKey, animeIDs, MALData) => {
     const dailyAnimeObj = {
         weekDay: weekDayKey,
         animeList: []
@@ -27,16 +25,42 @@ const populateAnimeListForWeekDay = (weekDayKey, animeIDs) => {
     return dailyAnimeObj;
 }
 
-const populateDailyAnimeSchedule = () => {
+const fetchMALData = async () => {
+    try {
+        const malResponse = await $fetch('https://api.myanimelist.net/v2/users/minhhikari1994/animelist', {
+            query: {
+                limit: 1000,
+                sort: 'list_updated_at',
+                fields: 'list_status,num_episodes',
+                nsfw: true
+            },
+            headers: {
+                'X-MAL-CLIENT-ID': process.env.MAL_API_KEY
+            }
+        })
+        return malResponse
+    } catch (err) {
+        console.error('======================Failed to fetch MAL data=======================')
+        console.error(err);
+        console.error('================= End Failed to fetch MAL data log===================')
+        return null
+    }
+}
+
+const populateDailyAnimeSchedule = async () => {
     const result = []
-    for (const [dayKey, animeIDs] of Object.entries(dailyAnimeIDs)) {
-        result.push(populateAnimeListForWeekDay(dayKey, animeIDs))
+    const malData = await fetchMALData()
+    if (malData) {
+        for (const [dayKey, animeIDs] of Object.entries(dailyAnimeIDs)) {
+            result.push(populateAnimeListForWeekDay(dayKey, animeIDs, malData))
+        }
     }
 
     return result
 }
 
-export default defineCronHandler('daily', () => {
-    const result = populateDailyAnimeSchedule()
+export default defineCronHandler('daily', async () => {
+    console.log('Populating daily anime schedule..,', new Date().toLocaleString())
+    const result = await populateDailyAnimeSchedule()
     fs.writeFileSync('./public/dailyAnimeSchedule.json', JSON.stringify(result), 'utf-8')
-}, { runOnInit: true })
+}, { runOnInit: true, timeZone: 'UTC+7' })
