b8089a1d66bf3cee1ce79aea4266cbb49fd0e2ea
Minh Hikari <minhhikari1994+dev@outlook.com>
2024-04-25 21:59:09 +0700

    Custom header for post

---
+++ components/content/ProseH1.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="box border-3 w-full mx-auto my-5 text-center custom-header">
+        <h1 class="text-2xl mb-2 text-white">
+            <slot />
+        </h1>
+    </div>
+</template>
+
+<style lang="scss">
+    .custom-header {
+        background: #24205a;
+    }
+</style>
+
